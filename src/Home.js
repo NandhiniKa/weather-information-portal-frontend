0042b98197a895838e01804650c223224eff8be7
NandhiniKa <nandhinik1234567890@gmail.com>
2024-03-11 16:38:41 +0530

    first commit

---
+++ src/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div className="home-container">
+      <h3>Welcome to React-App</h3>
+      <img className="home-img" src="https://www.ncei.noaa.gov/sites/g/files/anmtlf171/files/styles/max_1300x1300/public/sites/default/files/collage-of-weather-phenomena_1200x480.png?itok=PEeyMVbC" alt="Welcome picture" />
+    </div>
+  )
+}
---
+++ src/global.js
@@ -0,0 +1,1 @@
+export const API = "https://weather-information-portal-backend.vercel.app"
